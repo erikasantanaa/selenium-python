944128f8ed904ac250d311d5077eb47067a2afaf
erikasantanaa <erikamariasantanaa@gmail.com>
2022-11-18 00:23:28 -0300

    conclusão do teste com a página google, com behave

--- bdd/features/steps/google.py
+++ bdd/features/steps/google.py
@@ -1,1 +1,21 @@
-# from behave import given, when, then
+from behave import given, when, then
+
+@given(u'que acesso a pagina do google')
+def step_impl(context):
+    context.web.get("https://google.com.br")
+
+@when(u'realizo uma pesquisa')
+def step_impl(context):
+    context.element = driver.find_element("name", "q")
+    context.element.click()
+    context.element.send_keys("Marvel")
+    context.element.submit()
+
+@when(u'pesquisa e realizada')
+def step_impl(context):
+    assert context.web.title == "Marvel - Pesquisa Google"
+
+@then(u'o titulo da pagina deve ser validado')
+def step_impl(context):
+    pass
+
